0f2a8787386da2aaf7381639d1bd2067072e8629
sylvester snijders <sylvestersnijders007@gmail.com>
2023-03-30 13:39:54 +0200

    Added a basic store for races

--- my-app/src/routes/faction/+page.svelte
+++ my-app/src/routes/faction/+page.svelte
@@ -1,10 +1,15 @@
 <script>
     import Dropdown from "../Dropdown.svelte";
-import DropdownHolder from "../DropdownHolder.svelte";
+    import DropdownHolder from "../DropdownHolder.svelte";
 </script>
 
 
 <h1>These are all the factions!</h1>
 <a href="/units/greenskins">Greenskins</a>
 <a href="/units/empire">Empire</a>
-<DropdownHolder></DropdownHolder>
+<DropdownHolder
+factionNames = {["John", "James"]}
+factionIcons = {["https://via.placeholder.com/200","https://via.placeholder.com/200"]}
+raceName = "Others"
+/>
+<DropdownHolder/>
