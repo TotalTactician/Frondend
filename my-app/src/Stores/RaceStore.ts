ffc6757c8638f6db4c51df1bb51e77503a74f650
AsterosTheGreat <sylvestersnijders007@gmail.com>
2023-03-31 12:28:38 +0200

    Added store and start on server call

---
+++ my-app/src/Stores/RaceStore.ts
@@ -0,0 +1,13 @@
+import { writable } from 'svelte/store';
+
+interface Race {
+    raceName: string;
+    raceIcon: string;
+    factionNames: string[];
+    factionIcons: string[];
+}
+let race: Race = {raceName: "Dawi-Zharr", raceIcon: "https://via.placeholder.com/200", factionIcons: ["https://via.placeholder.com/20","https://via.placeholder.com/20"], factionNames: ["Metal Crushers","Drill masters"]}
+export let races: Race[] = [];
+races.push(race)
+race = {raceName: "Dawi", raceIcon: "https://via.placeholder.com/200", factionIcons: ["https://via.placeholder.com/20","https://via.placeholder.com/20"], factionNames: ["Ironbrow","Zhuffbar"]}
+races.push(race)
