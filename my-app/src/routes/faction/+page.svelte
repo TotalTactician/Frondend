fef74f88420fa6a8ec852811c3b011f78979d8a8
Joey Remmers <joeyjoeyremmers@gmail.com>
2023-05-10 14:47:29 +0200

    back-end connection
    we got the connection working and adding data to the store kinda works. But the rest is just, Pain, Agony and SUFFFERING.
    
    Co-Authored-By: Sylvester Snijders <39733159+AsterosTheGreat@users.noreply.github.com>

--- my-app/src/routes/faction/+page.svelte
+++ my-app/src/routes/faction/+page.svelte
@@ -2,17 +2,20 @@
     import Dropdown from "../Dropdown.svelte";
     import DropdownHolder from "../DropdownHolder.svelte";
     import { races } from "../../Stores/RaceStore";
+
 </script>
 
 
 <h1>These are all the factions!</h1>
 <a href="/units/greenskins">Greenskins</a>
 <a href="/units/empire">Empire</a>
-{#each races as race}
-  <DropdownHolder 
-    raceName = {race.raceName}
-    raceIcon = {race.raceIcon}
-    factionNames = {race.factionNames}
-    factionIcons = {race.factionIcons}
-  />
+{#each $races as race}
+{console.log(race)}
+<DropdownHolder 
+  raceName = {race.raceName}
+  raceIcon = {race.raceIcon}
+  factionNames = {race.factionNames}
+  factionIcons = {race.factionIcons}
+/>
 {/each}
+
