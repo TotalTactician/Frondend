d28f046251ee96b90931e2b5c8c172ae3e4a1d61
Bram Verkuijlen <bram.verkuijlen.bv@gmail.com>
2023-06-13 11:10:52 +0200

    🙄 Relentlessly Gathering Games! Unleashed the Unit store, constantly pestering the API to snatch up those precious games and relentlessly stash them in the store, leaving no stone unturned in our quest for gaming greatness! 😒🕵️‍♂️🎮📦

--- my-app/src/Stores/UnitStore.ts
+++ my-app/src/Stores/UnitStore.ts
@@ -1,4 +1,4 @@
-import { writable } from 'svelte/store';
+import {writable} from 'svelte/store';
 
 interface Unit {
     Name: string;
@@ -54,3 +54,80 @@ interface Unit {
     Mass: string;
     FatigueModifier: string;
 }
+
+const units= writable<Unit[]>([])
+
+const getRaces = async () => {
+    try {
+        const response = await fetch('[APICALLHERE!!!!!!]');
+        const result = await response.json();
+
+        if (!result)
+        {
+            result.Units.forEach((newUnit: {
+                Name: string;
+                Picture: string;
+                RaceId: string;
+                Health: string;
+                HealthPerModel: string;
+                Barrier: string;
+                Leadership: string;
+                CostSP: string;
+                UpkeepSP: string;
+                CostMP: string;
+                UpkeepMP: string;
+                ModelCount: string;
+                Armour: string;
+                MeleeDefence: string;
+                Parry: string;
+                WardSave: string;
+                PhysicalResist: string;
+                MissileResist: string;
+                MagicResist: string;
+                FireResist: string;
+                MeleeAttack: string;
+                MeleeIsFire: boolean;
+                MeleeIsMagic: boolean;
+                WeaponStrength: string;
+                BaseStrength: string;
+                APStrength: string;
+                VSLarge: string;
+                VSInfantry: string;
+                AttackSpeed: string;
+                HighThreat: boolean;
+                SplashTargetSize: string;
+                SplashMaxAttack: string;
+                Speed: string;
+                ChargeBonus: string;
+                Ammunation: string;
+                Range: string;
+                MissileStrength: string;
+                MissileBaseStrength: string;
+                MissileAPStrength: string;
+                MissileVSLarge: string;
+                MissileVSInfantry: string;
+                ExplosionBaseStrength: string;
+                ExplosionAPStrength: string;
+                DetonationRadius: string;
+                ShotsPerVolley: string;
+                ProjectileCount: string;
+                ReloadTime: string;
+                Accuracy: string;
+                CalibrationDistance: string;
+                CalibrationArea: string;
+                Mass: string;
+                FatigueModifier: string; }) => {
+
+                units.update( units => {
+                    units.push(newUnit)
+                    return units
+                })
+            })
+        }
+
+    }
+    catch {
+        return units
+    }
+}
+
